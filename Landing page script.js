9f08bced844a32dc13003b7da5b4958f13cd7fd9
AswanthTamilselvan <aswantht11@gmail.com>
2024-06-09 13:53:30 +0530

    Add files via upload

---
+++ Landing page script.js
@@ -0,0 +1,10 @@
+const heroButton = document.querySelector('#hero button');
+const pricingButton = document.querySelector('#pricing button');
+
+heroButton.addEventListener('click', () => {
+    console.log('Get Started button clicked!');
+});
+
+pricingButton.addEventListener('click', () => {
+    console.log('Sign Up button clicked!');
+});
